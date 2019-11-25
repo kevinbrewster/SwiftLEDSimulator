9eeb0e9f70e855719fab320354b3266b888c0173
Kevin Brewster <brewsterdesigns@gmail.com>
2019-11-25 11:55:16 -0500

    Initial Commit

---
+++ SwiftLEDSimulator/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SwiftLEDSimulator
+//
+//  Created by Kevin Brewster on 11/25/19.
+//  Copyright © 2019 Kevin Brewster. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
